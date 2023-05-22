6e7da69da6b83fbc3fd0fb260faa45a602b54f72
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-05-22 14:30:41 +0400

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [name,setName]=useState('mario');
+    const [age,setAge]=useState(25);
+
+    const handleClick=()=>{
+        setName('luigi');
+        setAge(30)
+    }
     return ( 
-        <div className="home">
+        <div clssName="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
