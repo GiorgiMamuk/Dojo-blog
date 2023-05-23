f2e71fe48e4d741bc8f006e948cf7e1771415cfc
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-05-23 16:38:00 +0400

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,16 @@
-import {useState} from 'react';
+import {useState} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name,setName]=useState('mario');
-    const [age,setAge]=useState(25);
-
-    const handleClick=()=>{
-        setName('luigi');
-        setAge(30)
-    }
+    const [blogs,setBlogs]=useState([
+        {title:'My new website', body:'lorem ipsum', author:'mario', id:1},
+        {title:'Welcome party', body:'lorem ipsum', author:'yoshi', id:2},
+        {title:'Web dev top tips', body:'lorem ipsum', author:'mario', id:3}
+    ]);
     return ( 
-        <div clssName="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-        </div>
+        <div className='home'>
+            <BlogList blogs={blogs} title="All Blogs!"/>
+         </div>
      );
 }
  
