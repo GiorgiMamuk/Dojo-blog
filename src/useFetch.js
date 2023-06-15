64b8500f7f1e340c2035d6d5fa8d8346d7ea5f3a
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-06-16 00:19:56 +0400

    last commit

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,8 +5,9 @@ const useFetch = (url) => {
     const [isPending,setIsPending]=useState(true);
     const [error,setError]=useState(null);
     useEffect(()=>{
+        const abortCont= new AbortController()
         setTimeout(()=>{
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
         .then (res=>{
             if(! res.ok){
                 throw Error ('could not fetch the data');
@@ -18,11 +19,16 @@ const useFetch = (url) => {
             setIsPending(false);
         })
         .catch(err=>{
+            if (err.name === 'AbortError') {
+                console.log('fetch aborted')
+            } else{
             setIsPending(false);
             setError(err.message);
-            setError(null);
+            
+        }
         })
     }, 1000);
+    return()=>abortCont.abort();
     },[url]);
     return {data, isPending, error}
 }
